ad26d26f0922eff17ea9c7cb000e87c0931ad6f5
ngesa marvin <marvinngesa@gmail.com>
2018-01-27 12:44:46 +0300

    set the onclick listeners and turn the image to grey

--- app/src/main/java/com/droidmarvin/floorlitearduino/FloorMainActivity.java
+++ app/src/main/java/com/droidmarvin/floorlitearduino/FloorMainActivity.java
@@ -3,15 +3,19 @@ package com.droidmarvin.floorlitearduino;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FloorMainActivity extends AppCompatActivity {
 
     private DatabaseReference mLedReference;
 
     private LinearLayout mLayoutA, mLayoutB, mLayoutC, mLayoutD;
+
+    private ImageView imageA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +27,17 @@ public class FloorMainActivity extends AppCompatActivity {
         mLayoutC = findViewById(R.id.light_c);
         mLayoutD = findViewById(R.id.light_c);
 
+        imageA.findViewById(R.id.image_a);
+
         mLayoutA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                mLedReference = FirebaseDatabase.getInstance().getReference().
+                        child("Users").child("ledStatus");
+                mLedReference.setValue(1);
 
+                imageA.setBackgroundDrawable(getResources().
+                        getDrawable(R.drawable.circlebackgroundgrey));
             }
         });
 

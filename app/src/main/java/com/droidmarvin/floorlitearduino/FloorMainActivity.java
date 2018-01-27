6d0145f15f220d15cdb47636b71247bfabfa9d51
ngesa marvin <marvinngesa@gmail.com>
2018-01-27 11:15:14 +0300

    arduino floorLite initial things commit 🚨

---
+++ app/src/main/java/com/droidmarvin/floorlitearduino/FloorMainActivity.java
@@ -0,0 +1,14 @@
+package com.droidmarvin.floorlitearduino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FloorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floor_main);
+    }
+}
+

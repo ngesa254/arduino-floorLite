4136b0cb4fb7e6e2696479176108e1757238b73d
ngesa marvin <marvinngesa@gmail.com>
2018-01-31 20:04:09 +0300

    Add Firebase Project URL and Database Secret

--- arduino/floorLite/floorLite.ino
+++ arduino/floorLite/floorLite.ino
@@ -2,7 +2,7 @@
 #include <ESP8266WiFi.h>
 #include<FirebaseArduino.h>
 
-#define FIREBASE_HOST "xxxxxxxxxxxxxxxxxx.firebaseio.com" //Your Firebase Project URL goes here without "http:" , "\" and "/"
+#define FIREBASE_HOST "xxxxxxxxxxxxxxxxxx.firebaseio.com" //Your Firebase Project URL goes here without "http:" , "\" and "/" 
 #define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" //Your Firebase Database Secret goes here
 
 void setup() {

ce2006e98f883b16107d28b4236f89b0546e43c9
ngesa marvin <marvinngesa@gmail.com>
2018-01-31 20:06:40 +0300

    add your WiFi SSID and Password for which NodeMCU connects

--- arduino/floorLite/floorLite.ino
+++ arduino/floorLite/floorLite.ino
@@ -5,6 +5,9 @@
 #define FIREBASE_HOST "xxxxxxxxxxxxxxxxxx.firebaseio.com" //Your Firebase Project URL goes here without "http:" , "\" and "/" 
 #define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" //Your Firebase Database Secret goes here
 
+#define WIFI_SSID "xxxxxxxxxx" //your WiFi SSID for which yout NodeMCU connects
+#define WIFI_PASSWORD "xxxxxxxxx"//Password of your wifi network 
+
 void setup() {
   // put your setup code here, to run once:
 
